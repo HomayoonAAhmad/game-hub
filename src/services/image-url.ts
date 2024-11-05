0962b96c77ff9a6e2802e3190df25e3d153ea256
HomayoonAAhmad <homayoon5600@gmail.com>
2024-11-05 10:04:47 +0330

    optimized image

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+import { renderIntoDocument } from "react-dom/test-utils";
+
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCroppedImageUrl;
